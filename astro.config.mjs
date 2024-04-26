c5ff542338c0d201a8d8d39ed54eec26cacf2ed5
FakuOrtiz <facundoagustinortiz@gmail.com>
2024-04-26 13:33:54 -0300

    chore: remove unnecesary comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()]
 });
