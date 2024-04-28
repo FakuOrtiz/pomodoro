073622f6b4b5d19658572e178ce5c0494a1c3fe1
FakuOrtiz <facundoagustinortiz@gmail.com>
2024-04-28 19:45:47 -0300

    feat: add @astropub/worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+import worker from "@astropub/worker";
 
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact(), worker(),]
 });
