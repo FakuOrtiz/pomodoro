f585f17c44d682788494a2028f20deec89c84459
FakuOrtiz <facundoagustinortiz@gmail.com>
2024-04-28 20:29:15 -0300

    remove: @astropub/worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-import worker from "@astropub/worker";
 
 export default defineConfig({
-  integrations: [tailwind(), preact(), worker(),]
+  integrations: [tailwind(), preact()]
 });
